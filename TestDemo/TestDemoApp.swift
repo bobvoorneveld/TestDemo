39117dcee266dcd31169e52e16d0497d91d5446b
Bob Voorneveld <info@bobvoorneveld.nl>
2024-10-16 17:10:44 +0200

    Initial Commit

---
+++ TestDemo/TestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoApp.swift
+//  TestDemo
+//
+//  Created by Bob Voorneveld on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestDemoTests/TestDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoTests.swift
+//  TestDemoTests
+//
+//  Created by Bob Voorneveld on 16/10/2024.
+//
+
+import Testing
+@testable import TestDemo
+
+struct TestDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
